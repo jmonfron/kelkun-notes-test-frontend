b9da4b20e1a2b1bf80ab26aabcf3550b623e7544
Jordan <mfrt.jordan@gmail.com>
2025-11-22 15:28:33 +0100

    remove duplicate file

--- src/services/schemas/projects/createUserSchema.ts
+++
@@ -1,6 +0,0 @@
-import {z} from 'zod'
-
-export const createUserSchema = z.object({
-    firstname: z.string().min(1, `Veuillez entrer le prénom`),
-    email: z.string().email(`Veuillez entrer une adresse email valide`)
-})
